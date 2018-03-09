7166e2c379330c7534abf93dd114c21ecbbbb2cb
Kevin Vargas <kvargas@hnsc.net>
2018-03-09 17:18:56 -0600

    API Communication

--- src/theme/Login.vue
+++ src/theme/Login.vue
@@ -1,45 +1,104 @@
 <template>
   <div class="content">
-    <h2>Login</h2>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Username</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-        <div class="control">
-          <input class="input" type="text"
-          placeholder="Your username">
-        </div>
-        </div>
-      </div>
+    <div v-if="isAuthenticated">
+      <h2>Hello Authenticated user!</h2>
+      <p>Name: {{profile.firstName}}</p>
+      <p>Favorite Sandwich: {{profile.favoriteSandwich}}</p>
+      <button class="button is-primary" v-on:click="logout()">Logout</button>
     </div>
-    <div class="field is-horizontal">
-      <div class="field-label is-normal">
-        <label class="label">Password</label>
-      </div>
-      <div class="field-body">
-        <div class="field">
-        <div class="control">
-          <input class="input" type="password"
-          placeholder="Your password">
+    <div v-else>
+      <h2>Login</h2>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Username</label>
         </div>
+        <div class="field-body">
+          <div class="field">
+          <div class="control">
+            <input class="input" type="text"
+            placeholder="Your username" v-model="username">
+          </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="field is-horizontal">
-      <div class="field-label">
-        <!-- Left empty for spacing -->
-      </div>
-      <div class="field-body">
-        <div class="field">
-        <div class="control">
-          <button class="button is-primary">
-          Login
-          </button>
+      <div class="field is-horizontal">
+        <div class="field-label is-normal">
+          <label class="label">Password</label>
         </div>
+        <div class="field-body">
+          <div class="field">
+          <div class="control">
+            <input class="input" type="password"
+            placeholder="Your password" v-model="password">
+          </div>
+          </div>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-label">
+          <!-- Left empty for spacing -->
+        </div>
+        <div class="field-body">
+          <div class="field">
+          <div class="control">
+            <button class="button is-primary" v-on:click="login()">
+            Login
+            </button>
+          </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+import appService from '../app.service.js'
+export default {
+  data () {
+    return {
+      username: '',
+      password: '',
+      isAuthenticated: false,
+      profile: {}
+    }
+  },
+  methods: {
+    login () {
+      appService.login({username: this.username, password: this.password})
+        .then((data) => {
+          window.localStorage.setItem('token', data.token)
+          window.localStorage.setItem('tokenExpiration', data.expiration)
+          this.isAuthenticated = true
+          this.username = ''
+          this.password = ''
+        })
+        .catch(() => window.alert('Could not login'))
+    },
+    logout () {
+      window.localStorage.setItem('token', null)
+      window.localStorage.setItem('tokenExpiration', null)
+      this.isAuthenticated = false
+    }
+  },
+  watch: {
+    isAuthenticated: function (val) {
+      if (val) {
+        appService.getProfile()
+          .then(profile => {
+            this.profile = profile
+          })
+      } else {
+        this.profile = {}
+      }
+    }
+  },
+  created () {
+    let expiration = window.localStorage.getItem('tokenExpiration')
+    var unixTimestamp = new Date().getTime() / 1000
+    if (expiration !== null && parseInt(expiration) - unixTimestamp > 0) {
+      this.isAuthenticated = true
+    }
+  }
+}
+</script>
+
